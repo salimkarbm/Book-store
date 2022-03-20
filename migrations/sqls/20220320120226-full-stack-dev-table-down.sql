092c04f70308d3d5427def1504115deb73a97c48
salim imuzai <imuzaidynasty@gmail.com>
2022-03-20 20:29:47 +0100

    add migration to confirm database connection

---
+++ migrations/sqls/20220320120226-full-stack-dev-table-down.sql
@@ -0,0 +1,2 @@
+/* Replace with your SQL commands */
+DROP TABLE full_stack_dev;
---
+++ migrations/sqls/20220320120226-full-stack-dev-table-up.sql
@@ -0,0 +1,2 @@
+/* Replace with your SQL commands */
+CREATE TABLE full_stack_dev (id serial primary key, name varchar(100), type varchar(50), weight integer);
